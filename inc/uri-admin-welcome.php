e23664e3c5af6fbcc3d0fd78ffd0a51592b68440
John Pennypacker <jpennypacker@uri.edu>
2021-04-30 09:00:14 -0400

    initial commit

---
+++ inc/uri-admin-welcome.php
@@ -0,0 +1,47 @@
+<div class="custom-welcome-panel postbox">
+
+	<h3><?php _e( 'You’re using URI WordPress.', 'uri' ); ?></h3>
+
+	<p class="about-description"><?php _e( 'Don’t Panic.', 'uri' ); ?></p>
+	
+	<div class="welcome-cols">
+	
+		<div class="column">
+			<h4><?php _e( 'Let\'s get started', 'uri' ); ?></h4>
+			<ul>
+			<?php if ( 'page' !== get_option( 'show_on_front' ) ) : ?>
+				<li><?php printf( '<a href="%s" class="welcome-icon welcome-get-started" target="_blank">' . __( 'Set up your front page', 'uri' ) . '</a>', 'https://wordpress.com/support/pages/front-page/' ); ?></li>
+			<?php else: ?>
+				<li><?php printf( '<a href="%s" class="welcome-icon welcome-edit-page">' . __( 'Edit your front page', 'uri' ) . '</a>', get_edit_post_link( get_option( 'page_on_front' ) ) ); ?></li>
+			<?php endif; ?>
+			<li><?php printf( '<a href="%s" class="welcome-icon welcome-add-page">' . __( 'Add a new page', 'uri' ) . '</a>', admin_url( 'post-new.php?post_type=page' ) ); ?></li>
+			<li><?php printf( '<a href="%s" class="welcome-icon welcome-view-site">' . __( 'View your site', 'uri' ) . '</a>', home_url( '/' ) ); ?></li>
+			<li><?php printf( __( '<a href="%s" target="_blank">How to create content</a>' ), 'https://make.wordpress.org/support/user-manual/content/' ); ?></li>
+		</div>
+		
+		<?php if ( current_user_can( 'customize' ) ): ?>
+		<div class="column">
+			<h4><?php _e( 'Next steps', 'uri' ); ?></h4>
+			<ul>
+				<li class="hide-if-no-customize"><?php printf( '<a href="%s" class="welcome-icon welcome-customize">' . __( 'Customize your site', 'uri' ) . '</a>', admin_url( 'customize.php' ) ); ?></li>
+				<li class="hide-if-no-customize"><?php printf( '<div class="welcome-icon welcome-widgets-menus">' . __( '<a href="%s">Manage menus</a>' ) . '</div>', admin_url( 'nav-menus.php' ) ); ?></li>
+				<li class="hide-if-no-customize"><?php printf( '<div class="welcome-icon welcome-widgets">' . __( '<a href="%s">Manage widgets</a>' ) . '</div>', admin_url( 'widgets.php' ) ); ?></li>
+				<li class="hide-if-no-customize"><?php printf( __( '<a href="%s">Edit your site settings</a>' ), admin_url( 'options-general.php' ) ); ?></li>				
+				<!--li><?php printf( __( '<a href="%s" target="_blank">About categories</a>' ), 'https://make.wordpress.org/support/user-manual/content/categories-and-tags/categories/' ); ?></li-->
+			</ul>
+		</div>
+		<?php endif; ?>
+
+		<div class="column">
+			<h4><?php _e( 'WordPress at URI', 'uri' ); ?></h4>
+			<ul>
+				<li><?php printf( '<a href="%s" class="welcome-icon welcome-wp-at-uri" target="_blank">' . __( 'About WordPress at URI', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/' ); ?></li>
+				<li><?php printf( '<a href="%s" class="welcome-icon welcome-content-guide" target="_blank">' . __( 'URI Content Guide', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/content-guide/' ); ?></li>
+				<li><?php printf( '<a href="%s" class="welcome-icon welcome-components" target="_blank">' . __( 'About URI components', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/components/' ); ?></li>
+				<li><?php printf( __( '<a href="%s" target="_blank">' . __( 'Get help', 'uri' ) . '</a>' ), 'https://www.uri.edu/wordpress/request/support/?your_site_url=' . urlencode( home_url( '/' ) ) . '&your_email=' . urlencode( wp_get_current_user()->user_email ) ); ?></li>
+			</ul>
+		</div>
+		
+	</div><!-- .welcome-cols -->
+
+</div><!-- .custom-welcome-panel -->
---
+++ uri-admin.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Plugin Name: URI Admin 
+ * Plugin URI: https://www.uri.edu/wordpress/software/
+ * Description: Customizations for the admin dashboard
+ * Version: 1.0
+ * Author: URI Web Communications
+ * Author URI: https://web.uri.edu/external-relations/contact-us/#web
+ *
+ * @author: John Pennypacker <jpennypacker@uri.edu>
+ * @author: Brandon Fuller <bjcfuller@uri.edu>
+ * @package uri-admin
+ */
+
+// Block direct requests
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+// prevent the confirm admin email prompt from appearing
+add_filter( 'admin_email_check_interval', '__return_false' );
+
+// Remove the default welcome dashboard message
+remove_action( 'welcome_panel', 'wp_welcome_panel' );
+
+
+function uri_admin_style() {	
+
+	if ( function_exists( 'get_plugin_data' ) ) {
+		$values = get_plugin_data( __FILE__, false );
+		$cache_buster = $values['Version'];
+	} else {
+		// cache for a day
+		$cache_buster = gmdate( 'Ymd', strtotime( 'now' ) );
+	}
+  wp_enqueue_style( 'uri-admin-styles', plugins_url( 'uri-admin.css', __FILE__ ), [], $cache_buster );
+}
+add_action('admin_enqueue_scripts', 'uri_admin_style');
+
+/**
+ * Override the default "Thank you for using WordPress admin footer".
+ */
+function uri_admin_footer() {
+	echo '<p class="footer-about-wp"><a href="https://www.uri.edu/wordpress/" target="_blank">Learn more about WordPress at URI</a>.</p>';
+}
+add_filter( 'admin_footer_text', 'uri_admin_footer' );
+
+
+/**
+ * Create custom dashboard widgets and resort the widgets to put URI on top.
+ */
+function uri_admin_dashboard_widgets() {
+	global $wp_meta_boxes;
+
+	wp_add_dashboard_widget(
+		'uri_admin_dashboard_updates_feed',
+		__('URI WordPress Updates'),
+		'uri_admin_dashboard_wordpress_updates_feed_output',
+		NULL, // control callback
+		NULL, // callback args
+		'normal', // context
+		'core' // priority
+	); 
+
+	// Get the regular dashboard widgets array 
+	// (which already has our new widget but appended at the end).
+	$default_dashboard = $wp_meta_boxes['dashboard']['normal']['core'];
+	 
+	// Backup and delete our new dashboard widget from the end of the array.
+	$uri_widget_backup = array( 'uri_admin_dashboard_updates_feed' => $default_dashboard['uri_admin_dashboard_updates_feed'] );
+	unset( $default_dashboard['uri_admin_dashboard_updates_feed'] );
+
+	// Merge the two arrays together so our widget is at the beginning.
+	$sorted_dashboard = array_merge( $uri_widget_backup, $default_dashboard );
+
+	// Save the sorted array back into the original metaboxes. 
+	$wp_meta_boxes['dashboard']['normal']['core'] = $sorted_dashboard;
+
+	
+}
+add_action('wp_dashboard_setup', 'uri_admin_dashboard_widgets');
+
+
+/**
+ * Display the content of the RSS updates.
+ */
+function uri_admin_dashboard_wordpress_updates_feed_output() {
+	echo '<div class="rss-widget uri-wordpress-updates">';
+	wp_widget_rss_output(
+		'https://www.uri.edu/wordpress/updates/feed/rss',
+		array(
+			'items' => 2,
+			'show_summary' => 1,
+			'show_author' => 0,
+			'show_date' => 1
+		)
+	);
+	echo '<p class="uri-admin-wp-updates-footer"><a href="https://www.uri.edu/wordpress/updates/">Previous updates »</a></p>';
+	echo '</div>';
+}
+
+
+
+/**
+ * Remove unwanted WP Admin Dashboard boxes
+ */
+function uri_admin_remove_boxes() {
+	// @see https://www.wpexplorer.com/customize-wordpress-admin-dashboard/
+	// limit our handed-down customizations to users who can't manage options
+	if ( ! current_user_can( 'manage_options' ) ) {
+		// these are the default WP meta boxes
+		remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+		remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' ); // wp events and news
+		remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
+//		remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' ); // what's on your mind?
+		remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+		remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+//		remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' ); // # pages, #posts, theme
+//		remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' ); // activity
+	}
+
+}
+add_action( 'admin_init', 'uri_admin_remove_boxes' );
+
+/**
+ * display a replacement for the welcome panel
+ * @see https://developer.wordpress.org/reference/functions/wp_welcome_panel/
+ */
+function uri_admin_welcome_panel() {
+	$screen = get_current_screen();
+	if( $screen->base == 'dashboard' ) {
+		ob_start();
+		include 'inc/uri-admin-welcome.php';
+		ob_end_flush();
+	}
+}
+add_action('admin_notices', 'uri_admin_welcome_panel');
