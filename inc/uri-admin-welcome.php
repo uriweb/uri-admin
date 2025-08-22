768b6dc3bf53aeebb40d0df34c233040b78840f8
alexandragauss <alexandra_gauss@uri.edu>
2025-08-21 20:52:34 -0400

    Edit links and categories

--- inc/uri-admin-welcome.php
+++ inc/uri-admin-welcome.php
@@ -65,43 +65,36 @@ if ( 'URI Responsive' == $current_theme_name || 'Themify Responz' == $current_th
 				<ul>
 					<li><?php printf( '<a href="%s" class="welcome-icon welcome-wp-at-uri" target="_blank">' . __( 'About WordPress at URI', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/' ); ?></li>
 					<li><?php printf( '<a href="%s" class="welcome-icon welcome-content-guide" target="_blank">' . __( 'URI Content Guide', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/content-guide/' ); ?></li>
-					<li><?php printf( __( '<a href="%s" target="_blank">' . __( 'Get help', 'uri' ) . '</a>' ), 'https://www.uri.edu/wordpress/request/support/?your_site_url=' . urlencode( home_url( '/' ) ) . '&your_email=' . urlencode( wp_get_current_user()->user_email ) ); ?></li>
-				</ul>
+</ul>
 			</div>
 
 			<div class="column-admin">
 				<h4 class="column-head"><?php _e( 'How To', 'uri' ); ?></h4>
 				<ul>
+					<li><?php printf( '<a href="%s" class="welcome-icon welcome-accessibility" target="_blank">' . __( 'Make Your Site Accessible', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/content-guide/accessibility/' ); ?></li>
+			
 					<li><?php printf( '<a href="%s" class="welcome-icon welcome-components" target="_blank">' . __( 'Use URI Components', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/components/' ); ?></li>
-				<?php if ( 'page' !== get_option( 'show_on_front' ) ) : ?>
-					<li><?php printf( '<a href="%s" class="welcome-icon welcome-get-started" target="_blank">' . __( 'Set up your front page', 'uri' ) . '</a>', 'https://wordpress.com/support/pages/front-page/' ); ?></li>
-				<?php else: ?>
-					<li><?php printf( '<a href="%s" class="welcome-icon welcome-edit-page">' . __( 'Edit your front page', 'uri' ) . '</a>', get_edit_post_link( get_option( 'page_on_front' ) ) ); ?></li>
-				<?php endif; ?>
-				<li><?php printf( '<a href="%s" class="welcome-icon welcome-components" target="_blank">' . __( 'Write for the Web', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/content-guide/writing/' ); ?></li>
-			</div>
+</ul>
+				</div>
 
 			<div class="column-admin">
-				<h4 class="column-head"><?php _e( 'Media Library', 'uri' ); ?></h4>
+				<h4 class="column-head"><?php _e( 'Using the Media Library', 'uri' ); ?></h4>
 				<ul>
 					<li><?php printf( '<a href="%s" class="welcome-icon welcome-images">' . __( 'Images', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/content-guide/images/' ); ?></li>
 					<li><?php printf( '<a href="%s" class="welcome-icon welcome-view-site">' . __( 'Documents', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/content-guide/documents/' ); ?></li>
 				</ul>
 			</div>
 
-			<?php if ( current_user_can( 'edit_theme_options' ) ): ?>
-				
-			<div class="column-admin">
-				<h4 class="column-head"><?php _e( 'Advanced tasks', 'uri' ); ?></h4>
-				<ul>
-				<li class="hide-if-no-customize"><?php printf( '<a href="%s" class="welcome-icon welcome-customize">' . __( 'Customize your site', 'uri' ) . '</a>', admin_url( 'customize.php' ) ); ?></li>
-					<li class="hide-if-no-customize"><?php printf( '<div class="welcome-icon welcome-widgets-menus">' . __( '<a href="%s">Manage menus</a>' ) . '</div>', admin_url( 'nav-menus.php' ) ); ?></li>
-					<!--li><?php printf( __( '<a href="%s" target="_blank">Learn about categories</a>' ), 'https://make.wordpress.org/support/user-manual/content/categories-and-tags/categories/' ); ?></li-->
-				</ul>
-			</div>
-			<?php endif; ?>
+</div><!-- .welcome-cols -->
 
-		</div><!-- .welcome-cols -->
+		<div class="support-column">
+		<h4 class="column-head"><?php _e( 'Get Help', 'uri' ); ?></h4>
+		<ul>
+			<li class="support-link"><?php printf( '<a href="%s" class="welcome-icon welcome-common-issues">' . __( 'Troubleshoot Common Issues', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/request/support/' ); ?></li>
+			<li class="support-link"><?php printf( '<a href="%s" class="welcome-icon welcome-support">' . __( 'Request Support', 'uri' ) . '</a>', 'https://www.uri.edu/wordpress/request/' ); ?></li>
+		</ul>
+			</div>
+
 
 		<hr>
 
